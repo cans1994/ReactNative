efabdb10cf6c3b45862760fba2ac833364608ad8
[cans1994] <cans.bl@hotmail.com>
2022-03-15 14:25:59 +0100

    commit

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import Search from './Components/Search'
 
-export default function App() {
+export default class App extends React.Component {
+  render() {
+    return <Search />
+  }
+}
+
+
+/*export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Bonjour</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+}*/
 
 const styles = StyleSheet.create({
   container: {
---
+++ components/Search.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { View, TextInput, Button } from 'react-native'
+
+
+class Search extends React.Component {
+    render() {
+        return (
+        <View style={{ marginTop: 20 }}>
+                <TextInput
+  style={{
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5,
+  }}
+  placeholder="Titre du film"
+/>
+                <Button title='Rechercher' onPress={() => { }} />
+            </View>
+        )
+    }
+}
+
+export default Search
