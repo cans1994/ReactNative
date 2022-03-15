0d129e8280d14b1a62a879a346ad4a3e08e41cb8
[cans1994] <cans.bl@hotmail.com>
2022-03-15 14:40:48 +0100

    stylesheet

--- components/Search.js
+++ components/Search.js
@@ -1,6 +1,30 @@
 import React from 'react'
-import { View, TextInput, Button } from 'react-native'
+import { View, TextInput, Button, StyleSheet } from 'react-native'
 
+
+const styles = StyleSheet.create({
+  main_container: {
+    marginTop: 20,
+    flex: 1,
+  },
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5,
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 100,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
 
 class Search extends React.Component {
     render() {
