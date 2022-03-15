7ab98475f244f845dbd9aa3859a07483498cd913
[cans1994] <cans.bl@hotmail.com>
2022-03-15 16:23:59 +0100

    image logo

--- components/FilmItem.js
+++ components/FilmItem.js
@@ -1,17 +1,30 @@
 //Components/FilmItem.js
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 
 
 class FilmItem extends React.Component {
     render() {
         const film = this.props.film
         console.log(this.props.film.title)
-        return (<View>
+        return (
+            <View>
             <Text>{film.title}</Text>
+                <Image style={styles.image}
+                    source={{
+                        uri: 'https://reactnative.dev/img/tiny_logo.png',
+                    }} />
         </View>
         )
  }
 }
+
+const styles = StyleSheet.create ({
+    image: {
+        width: 20,
+        height: 20,
+
+    },
+})
 
 export default FilmItem
