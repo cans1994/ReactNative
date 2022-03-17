e78ad0ebb8275e03499c85a9f5f4af470d9a5f26
[cans1994] <cans.bl@hotmail.com>
2022-03-17 08:47:29 +0100

    json pretty

--- components/FilmItem.js
+++ components/FilmItem.js
@@ -1,14 +1,18 @@
 //Components/FilmItem.js
 import React from 'react'
 import { View, Text, Image, StyleSheet } from 'react-native'
+import 'react-json-pretty/themes/adventure_time.css'
+import JSONPretty from 'react-json-pretty'
+
 
 
 class FilmItem extends React.Component {
     render() {
         const film = this.props.film
-        console.log(this.props.film.title)
+        console.log('test:' + this.props.film.title)
         return (
             <View>
+                <JSONPretty data={film}></JSONPretty>
             <Text>{film.title}</Text>
                 <Image style={styles.image}
                     source={{
