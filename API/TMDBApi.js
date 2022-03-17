1692d4464fbd07f4c450140312a396df7a1a5787
[cans1994] <cans.bl@hotmail.com>
2022-03-17 15:58:07 +0100

    props et states

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,10 +1,23 @@
 // API/TMDBApi.js
 import axios from 'axios';
 import { API_TOKEN } from '@env'
+import getFilmsFromApiWithSearchedText from '../API/TMDBApi'
+
 
 const getFilmsFromApiWithSearchedText = async (text) => {
-  const url = 'https://api.themoviedb.org/3/search/movie?api_key=API_TOKEN' + API_TOKEN + '&language=fr&query=' + text
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=API_TOKEN' +
+        API_TOKEN +
+        '&language=fr&query=' +
+        text
+    const response = await axios.get(url)
+  console.log('--getFilmsFromApiWithSearchedText--')
+  console.log(url)
+  console.log(response.data)
+  console.log('--fin getFilmsFromApiWithSearchedText--')
+  return response.data
 
 }
+
+
 
 export default getFilmsFromApiWithSearchedText
--- components/Search.js
+++ components/Search.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { FlatList, View, TextInput, Button, StyleSheet} from 'react-native'
 import films from '../Helpers/filmsData'
 import FilmItem from './FilmItem'
+//import getFilmsFromApiWithSearchedText from '../API/TMDBApi'
 
 
 
@@ -41,9 +42,27 @@ class Search extends React.Component {
               renderItem={({ item }) => <FilmItem film={item} />} />
 
           </View>
+      )
+  }
+  constructor(props) {
+    super(props)
+    this.searchedText = ''
+    this.state = { films: [] }// ceci va devenir un state
+  }
 
-        )
-    }
+  // Nouvelle méthode
+  _searchTextInputChanged(text) {
+    this.searchedText = text // Modification du texte recherché à chaque saisie de texte, sans passer par setState
+  }
+
+  _loadFilms() {
+    getFilmsFromApiWithSearchedText(this.searchedText).then((data) => {
+      this.setState({ films: data.results })
+      // this.forceUpdate() // on va remplacer ceci par un state
+    })
+  }
 }
+
+
 
 export default Search
