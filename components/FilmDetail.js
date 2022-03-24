cdc21deaaf60fb92738c615da4c522518caec8c6
[cans1994] <cans.bl@hotmail.com>
2022-03-24 15:41:22 +0100

    correction du fichier filmdetail

--- components/FilmDetail.js
+++ components/FilmDetail.js
@@ -1,8 +1,24 @@
 import React from "react";
-import { StyleSheet, View, Text, ActivityIndicator } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  ActivityIndicator,
+  ScrollView,
+} from "react-native";
 import { getFilmDetailFromApi } from "../API/TMDBApi";
+import "react-json-pretty/themes/adventure_time.css";
+import JSONPretty from "react-json-pretty";
 
 class FilmDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      film: undefined, // Pour l'instant on n'a pas les infos du film, on initialise donc le film à undefined.
+      isLoading: true, // A l'ouverture de la vue, on affiche le chargement, le temps de récupérer le détail du film
+    };
+  }
+
   componentDidMount() {
     console.log("Component FilmDetail monté");
     getFilmDetailFromApi(this.props.navigation.getParam("idFilm")).then(
@@ -23,23 +39,6 @@ class FilmDetail extends React.Component {
     console.log("Component FilmDetail componentWillUnmount");
   }
 
-  render() {
-    const { idFilm } = this.props.navigation.state.params;
-    console.log("Component FilmDetail rendu idFilm = " + idFilm);
-    return (
-      <View style={styles.main_container}>
-        <Text>Détail du film</Text>
-      </View>
-    );
-  }
-  constructor(props) {
-    super(props);
-    this.state = {
-      film: undefined, // Pour l'instant on n'a pas les infos du film, on initialise donc le film à undefined.
-      isLoading: true, // A l'ouverture de la vue, on affiche le chargement, le temps de récupérer le détail du film
-    };
-  }
-
   _displayLoading() {
     if (this.state.isLoading) {
       // Si isLoading vaut true, on affiche le chargement à l'écran
@@ -51,12 +50,33 @@ class FilmDetail extends React.Component {
     }
   }
 
+  _displayFilm() {
+    if (this.state.film != undefined) {
+      return (
+        <ScrollView style={styles.scrollview_container}>
+          <Text>{this.state.film.title}</Text>
+          <JSONPretty data={this.state.film}></JSONPretty>
+        </ScrollView>
+      );
+    }
+  }
   render() {
-    const { idFilm } = this.props.navigation.getParam("idFilm");
+    const { idFilm } = this.props.navigation.state.params;
+    console.log("Component FilmDetail rendu idFilm = " + idFilm);
     return (
       <View style={styles.main_container}>
         {this._displayLoading()}
         <Text>Détail du film id {idFilm}</Text>
+        {this._displayFilm()}
+      </View>
+    );
+  }
+}
+/* render() {
+    const { idFilm } = this.props.navigation.getParam("idFilm");
+    return (
+      <View style={styles.main_container}>
+        
       </View>
     );
   }
@@ -69,7 +89,7 @@ const SearchStackNavigator = createStackNavigator({
   FilmDetail: {
     screen: FilmDetail,
   },
-});
+});*/
 
 const styles = StyleSheet.create({
   main_container: {
